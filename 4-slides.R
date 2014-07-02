895d76b0488bc851417f714a6bbf85bc4d1db4bc
edouard-legoupil <edouard.legoupil@gmail.com>
2014-07-02 15:03:42 +0300

    Add presentation

--- 4-slides.R
+++ 4-slides.R
@@ -5,7 +5,9 @@ install_github('slidify', 'ramnathv')
 install_github('slidifyLibraries', 'ramnathv')
 
 library(slidify)
+
+# Creating now the folder where the presentation will be developped
 author("presentation")
 
-## Now create the slides
+## Now create the slides from the mark down file
 slidify("index.Rmd")
